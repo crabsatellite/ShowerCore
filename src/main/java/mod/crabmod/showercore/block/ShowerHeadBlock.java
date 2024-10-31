62e951c343da35d6c144c0d0192be2c82cdca879
Alex Lee <islicy@outlook.com>
2024-10-31 03:11:25 -0400

    Support menu

--- src/main/java/mod/crabmod/showercore/block/ShowerHeadBlock.java
+++ src/main/java/mod/crabmod/showercore/block/ShowerHeadBlock.java
@@ -4,6 +4,9 @@ import java.awt.*;
 import mod.crabmod.showercore.base.RotatableBlock;
 import mod.crabmod.showercore.entity.ShowerHeadContainerEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
@@ -40,15 +43,49 @@ public class ShowerHeadBlock extends RotatableBlock implements EntityBlock {
       Player player,
       InteractionHand hand,
       BlockHitResult hit) {
+
+    boolean isShiftDown = player.isShiftKeyDown();
+
     if (level.isClientSide) {
-      return InteractionResult.SUCCESS;
+      if (!isShiftDown) {
+        for (int i = 0; i < 20; i++) {
+          double offsetX = 0.5 + (level.random.nextDouble() - 0.5) * 0.6;
+          double offsetY = -0.2;
+          double offsetZ = 0.5 + (level.random.nextDouble() - 0.5) * 0.6;
+          level.addParticle(
+              ParticleTypes.RAIN,
+              pos.getX() + offsetX,
+              pos.getY() + offsetY,
+              pos.getZ() + offsetZ,
+              0,
+              -0.1,
+              0);
+        }
+
+        level.playLocalSound(
+            pos.getX(),
+            pos.getY(),
+            pos.getZ(),
+            SoundEvents.WEATHER_RAIN,
+            SoundSource.BLOCKS,
+            1.0F,
+            1.0F,
+            false);
+
+        return InteractionResult.SUCCESS;
+      } else {
+        return InteractionResult.PASS;
+      }
     }
 
-    BlockEntity blockEntity = level.getBlockEntity(pos);
-    if (blockEntity instanceof ShowerHeadContainerEntity) {
-      player.openMenu((MenuProvider) blockEntity);
-      return InteractionResult.CONSUME;
+    if (isShiftDown) { // Shift + 右键：打开Menu
+      BlockEntity blockEntity = level.getBlockEntity(pos);
+      if (blockEntity instanceof ShowerHeadContainerEntity) {
+        player.openMenu((MenuProvider) blockEntity);
+        return InteractionResult.CONSUME;
+      }
     }
+
     return InteractionResult.PASS;
   }
 }
