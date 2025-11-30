87b1b9f80b0033ef0ad63d54dbf5f4255fad8e9c
Alex Li <islicy@outlook.com>
2025-11-30 16:49:52 +0900

    Implement waterlogging functionality for ShowerHeadBlock: add WATERLOGGED state, update block state definition, and handle fluid state interactions.

--- src/main/java/mod/crabmod/showercore/block/ShowerHeadBlock.java
+++ src/main/java/mod/crabmod/showercore/block/ShowerHeadBlock.java
@@ -32,11 +32,48 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import mod.crabmod.showercore.registers.BlockEntitiesRegister;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.LevelAccessor;
 
-public class ShowerHeadBlock extends RotatableBlock implements EntityBlock {
+public class ShowerHeadBlock extends RotatableBlock implements EntityBlock, SimpleWaterloggedBlock {
+  public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
   public ShowerHeadBlock(Properties properties) {
     super(properties);
+    this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, false));
+  }
+
+  @Override
+  protected void createBlockStateDefinition(StateDefinition.Builder<net.minecraft.world.level.block.Block, BlockState> builder) {
+    super.createBlockStateDefinition(builder);
+    builder.add(WATERLOGGED);
+  }
+
+  @Nullable
+  @Override
+  public BlockState getStateForPlacement(BlockPlaceContext context) {
+    FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
+    return super.getStateForPlacement(context).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+  }
+
+  @Override
+  public FluidState getFluidState(BlockState state) {
+    return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+  }
+
+  @Override
+  public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos currentPos, BlockPos neighborPos) {
+    if (state.getValue(WATERLOGGED)) {
+      level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+    }
+    return super.updateShape(state, direction, neighborState, level, currentPos, neighborPos);
   }
 
   @javax.annotation.Nullable
