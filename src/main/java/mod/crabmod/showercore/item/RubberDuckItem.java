017f325ca71aac8f14da1a73e36ed294f97e7f10
Alex Li <islicy@outlook.com>
2025-12-01 21:19:09 +0900

    fix: Refactor RubberDuckItem use method for improved entity placement logic

--- src/main/java/mod/crabmod/showercore/item/RubberDuckItem.java
+++ src/main/java/mod/crabmod/showercore/item/RubberDuckItem.java
@@ -1,34 +1,71 @@
 package mod.crabmod.showercore.item;
 
 import mod.crabmod.showercore.entity.RubberDuckEntity;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySelector;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 public class RubberDuckItem extends Item {
+    private static final Predicate<Entity> ENTITY_PREDICATE = EntitySelector.NO_SPECTATORS.and(Entity::isPickable);
+
     public RubberDuckItem(Properties properties) {
         super(properties);
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Level level = context.getLevel();
-        if (!(level instanceof ServerLevel)) {
-            return InteractionResult.SUCCESS;
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        HitResult hitresult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.ANY);
+        if (hitresult.getType() == HitResult.Type.MISS) {
+            return InteractionResultHolder.pass(itemstack);
         } else {
-            BlockPos blockpos = context.getClickedPos();
-            Direction direction = context.getClickedFace();
-            BlockPos blockpos1 = blockpos.relative(direction);
-            
-            RubberDuckEntity duck = new RubberDuckEntity(level, blockpos1.getX() + 0.5, blockpos1.getY(), blockpos1.getZ() + 0.5);
-            level.addFreshEntity(duck);
-            
-            context.getItemInHand().shrink(1);
-            return InteractionResult.CONSUME;
+            Vec3 vec3 = player.getViewVector(1.0F);
+            List<Entity> list = level.getEntities(player, player.getBoundingBox().expandTowards(vec3.scale(5.0D)).inflate(1.0D), ENTITY_PREDICATE);
+            if (!list.isEmpty()) {
+                Vec3 vec31 = player.getEyePosition();
+
+                for(Entity entity : list) {
+                    AABB aabb = entity.getBoundingBox().inflate((double)entity.getPickRadius());
+                    if (aabb.contains(vec31)) {
+                        return InteractionResultHolder.pass(itemstack);
+                    }
+                }
+            }
+
+            if (hitresult.getType() == HitResult.Type.BLOCK) {
+                RubberDuckEntity duck = new RubberDuckEntity(level, hitresult.getLocation().x, hitresult.getLocation().y, hitresult.getLocation().z);
+                duck.setYRot(player.getYRot());
+                if (!level.noCollision(duck, duck.getBoundingBox())) {
+                    return InteractionResultHolder.fail(itemstack);
+                } else {
+                    if (!level.isClientSide) {
+                        level.addFreshEntity(duck);
+                        level.gameEvent(player, GameEvent.ENTITY_PLACE, hitresult.getLocation());
+                        if (!player.getAbilities().instabuild) {
+                            itemstack.shrink(1);
+                        }
+                    }
+
+                    player.awardStat(Stats.ITEM_USED.get(this));
+                    return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
+                }
+            } else {
+                return InteractionResultHolder.pass(itemstack);
+            }
         }
     }
 }
