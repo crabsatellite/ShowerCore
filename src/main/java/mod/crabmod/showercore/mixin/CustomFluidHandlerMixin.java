bcc0106d6c7dd8b6ac8e0c8f0bd7d36df9d8ee1b
Alex Li <islicy@outlook.com>
2025-12-13 17:50:19 +0900

    fix: Update mixin imports and configuration for CustomFluidHandlerMixin; add local jar dependencies

--- src/main/java/mod/crabmod/showercore/mixin/CustomFluidHandlerMixin.java
+++ src/main/java/mod/crabmod/showercore/mixin/CustomFluidHandlerMixin.java
@@ -1,6 +1,6 @@
 package mod.crabmod.showercore.mixin;
 
-// import com.crabmod.hotbath.util.CustomFluidHandler;
+import com.crabmod.hotbath.util.CustomFluidHandler;
 import mod.crabmod.showercore.block.BathtubBlock;
 import mod.crabmod.showercore.utils.CoreUtils;
 import net.minecraft.world.entity.Entity;
@@ -10,15 +10,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// @Mixin(CustomFluidHandler.class)
+@Mixin(CustomFluidHandler.class)
 public class CustomFluidHandlerMixin {
 
-    /*
     @Inject(method = "isPlayerInHotBathBlock", at = @At("HEAD"), cancellable = true, remap = false)
     private static void isPlayerInHotBathBlock(Player player, CallbackInfoReturnable<Boolean> cir) {
         if (CoreUtils.isEntityInAnyHotBathtub(player)) {
             cir.setReturnValue(true);
         }
     }
-    */
 }
