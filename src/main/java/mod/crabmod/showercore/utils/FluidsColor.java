710ef3772bb3a9b1914f5584fef98fdd2d288fec
Alex Li <islicy@outlook.com>
2025-12-15 20:12:19 +0900

    refactor: Update FluidsColor to reference external fluid color constants

--- src/main/java/mod/crabmod/showercore/utils/FluidsColor.java
+++ src/main/java/mod/crabmod/showercore/utils/FluidsColor.java
@@ -1,10 +1,10 @@
 package mod.crabmod.showercore.utils;
 
 public class FluidsColor {
-    public static final int HOT_WATER_COLOR = 0xFF42A5F5;
-    public static final int HERBAL_BATH_COLOR = 0xFF66BB6A;
-    public static final int HONEY_BATH_COLOR = 0xFFFFA726;
-    public static final int MILK_BATH_COLOR = 0xFFFFFFFF;
-    public static final int PEONY_BATH_COLOR = 0xFFEC407A;
-    public static final int ROSE_BATH_COLOR = 0xFFEF5350;
+    public static final int HOT_WATER_COLOR = com.crabmod.hotbath.fluid_details.FluidsColor.HOT_WATER_COLOR;
+    public static final int HERBAL_BATH_COLOR = com.crabmod.hotbath.fluid_details.FluidsColor.HERBAL_BATH_COLOR;
+    public static final int HONEY_BATH_COLOR = com.crabmod.hotbath.fluid_details.FluidsColor.HONEY_BATH_COLOR;
+    public static final int MILK_BATH_COLOR = com.crabmod.hotbath.fluid_details.FluidsColor.MILK_BATH_COLOR;
+    public static final int PEONY_BATH_COLOR = com.crabmod.hotbath.fluid_details.FluidsColor.PEONY_BATH_COLOR;
+    public static final int ROSE_BATH_COLOR = com.crabmod.hotbath.fluid_details.FluidsColor.ROSE_BATH_COLOR;
 }
